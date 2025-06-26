93b1f131ceaabc7037842fcb8b189487dd8ec4bf
unknown <ayanansari771096@gmail.com>
2025-06-26 20:50:48 +0530

    Updated files

--- FRONTEND/src/helper/axiosInstance.js
+++ FRONTEND/src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "htts://food-donation-system-w44b.onrender.com/api/",
+  baseURL: "https://food-donation-system-w44b.onrender.com/api/",
   withCredentials: true,
   timeout: 10000,
 });
