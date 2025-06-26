0b92dc3315f7ade310651826fad8e91c280b3bd8
Ayan77-10 <98248222+Ayan77-10@users.noreply.github.com>
2025-06-26 20:23:26 +0530

    Update axiosInstance.js

--- FRONTEND/src/helper/axiosInstance.js
+++ FRONTEND/src/helper/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL:"https://food-donation-system-w44b.onrender.com,
   withCredentials: true,
   timeout: 10000,
 });
 
 axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: "https://food-donation-system-w44b.onrender.com,
 });
