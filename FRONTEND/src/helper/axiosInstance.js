282140393a76ec4510635b9263339909cec8ef1a
Ayan77-10 <98248222+Ayan77-10@users.noreply.github.com>
2025-06-26 20:23:51 +0530

    Update axiosInstance.js

--- FRONTEND/src/helper/axiosInstance.js
+++ FRONTEND/src/helper/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"https://food-donation-system-w44b.onrender.com,
+  baseURL:"https://food-donation-system-w44b.onrender.com",
   withCredentials: true,
   timeout: 10000,
 });
 
 axios.create({
-  baseURL: "https://food-donation-system-w44b.onrender.com,
+  baseURL: "https://food-donation-system-w44b.onrender.com",
 });
