c4914afefd9f51dd51359abc172240a7b090f652
Ayan77-10 <98248222+Ayan77-10@users.noreply.github.com>
2025-06-26 20:18:07 +0530

    Update axiosInstance.js

--- FRONTEND/src/helper/axiosInstance.js
+++ FRONTEND/src/helper/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.VITE_API_BASE_URL,
   withCredentials: true,
   timeout: 10000,
 });
 
 axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.VITE_API_BASE_URL,
 });
